70966bfd300fe4722a04f53e3b4bf8e3f58102e8
phillipivan <phillip.pietruschka@parliament.vic.gov.au>
2024-05-13 10:09:53 +1000

    Add volume up, volume down, mode presets Improve mute preset companion-module-base > 1.8.0 version bump > 1.2.1

--- src/presets.js
+++ src/presets.js
@@ -7,8 +7,8 @@ module.exports = async function (self) {
 		category: 'Audio',
 		name: `Mute`,
 		style: {
-			text: `Mute`,
-			size: 'auto',
+			text: `Volume: $(generic-module:volume)\\nMute`,
+			size: '14',
 			color: combineRgb(255, 255, 255),
 			bgcolor: combineRgb(0, 0, 0),
 			show_topbar: 'default',
@@ -19,7 +19,7 @@ module.exports = async function (self) {
 					{
 						actionId: 'mute',
 						options: {
-							mute: false,
+							mute: true,
 						},
 					},
 				],
@@ -30,7 +30,7 @@ module.exports = async function (self) {
 					{
 						actionId: 'mute',
 						options: {
-							mute: true,
+							mute: false,
 						},
 					},
 				],
@@ -44,6 +44,239 @@ module.exports = async function (self) {
 				style: {
 					color: combineRgb(0, 0, 0),
 					bgcolor: combineRgb(255, 0, 0),
+				},
+			},
+		],
+	}
+	presets[`volume_down`] = {
+		type: 'button',
+		category: 'Audio',
+		name: `Volume Down`,
+		style: {
+			text: `-`,
+			size: 'auto',
+			color: combineRgb(255, 255, 255),
+			bgcolor: combineRgb(0, 0, 0),
+			show_topbar: 'default',
+		},
+		steps: [
+			{
+				down: [
+					{
+						actionId: 'volumeDown',
+						options: {},
+					},
+				],
+				up: [],
+			},
+		],
+		feedbacks: [],
+	}
+	presets[`volume_up`] = {
+		type: 'button',
+		category: 'Audio',
+		name: `Volume Up`,
+		style: {
+			text: `+`,
+			size: 'auto',
+			color: combineRgb(255, 255, 255),
+			bgcolor: combineRgb(0, 0, 0),
+			show_topbar: 'default',
+		},
+		steps: [
+			{
+				down: [
+					{
+						actionId: 'volumeUp',
+						options: {},
+					},
+				],
+				up: [],
+			},
+		],
+		feedbacks: [],
+	}
+	presets[`mode_off`] = {
+		type: 'button',
+		category: 'Mode',
+		name: `Off`,
+		style: {
+			text: `Off`,
+			size: '14',
+			color: combineRgb(255, 255, 255),
+			bgcolor: combineRgb(0, 0, 0),
+			show_topbar: 'default',
+		},
+		steps: [
+			{
+				down: [
+					{
+						actionId: 'currentMode',
+						options: {
+							mode: 'off',
+						},
+					},
+				],
+				up: [],
+			},
+		],
+		feedbacks: [
+			{
+				feedbackId: 'currentMode',
+				options: {
+					mode: 'off',
+				},
+				style: {
+					color: combineRgb(0, 0, 0),
+					bgcolor: combineRgb(255, 0, 0),
+				},
+			},
+		],
+	}
+	presets[`mode_av`] = {
+		type: 'button',
+		category: 'Mode',
+		name: `AV`,
+		style: {
+			text: `AV`,
+			size: '14',
+			color: combineRgb(255, 255, 255),
+			bgcolor: combineRgb(0, 0, 0),
+			show_topbar: 'default',
+		},
+		steps: [
+			{
+				down: [
+					{
+						actionId: 'currentMode',
+						options: {
+							mode: 'av',
+						},
+					},
+				],
+				up: [],
+			},
+		],
+		feedbacks: [
+			{
+				feedbackId: 'currentMode',
+				options: {
+					mode: 'av',
+				},
+				style: {
+					color: combineRgb(0, 0, 0),
+					bgcolor: combineRgb(0, 204, 0),
+				},
+			},
+		],
+	}
+	presets[`mode_browser`] = {
+		type: 'button',
+		category: 'Mode',
+		name: `Browser`,
+		style: {
+			text: `Browser`,
+			size: '14',
+			color: combineRgb(255, 255, 255),
+			bgcolor: combineRgb(0, 0, 0),
+			show_topbar: 'default',
+		},
+		steps: [
+			{
+				down: [
+					{
+						actionId: 'currentMode',
+						options: {
+							mode: 'browser',
+						},
+					},
+				],
+				up: [],
+			},
+		],
+		feedbacks: [
+			{
+				feedbackId: 'currentMode',
+				options: {
+					mode: 'browser',
+				},
+				style: {
+					color: combineRgb(0, 0, 0),
+					bgcolor: combineRgb(0, 204, 0),
+				},
+			},
+		],
+	}
+	presets[`mode_splash`] = {
+		type: 'button',
+		category: 'Mode',
+		name: `Splash`,
+		style: {
+			text: `Splash`,
+			size: '14',
+			color: combineRgb(255, 255, 255),
+			bgcolor: combineRgb(0, 0, 0),
+			show_topbar: 'default',
+		},
+		steps: [
+			{
+				down: [
+					{
+						actionId: 'currentMode',
+						options: {
+							mode: 'splash',
+						},
+					},
+				],
+				up: [],
+			},
+		],
+		feedbacks: [
+			{
+				feedbackId: 'currentMode',
+				options: {
+					mode: 'splash',
+				},
+				style: {
+					color: combineRgb(0, 0, 0),
+					bgcolor: combineRgb(0, 204, 0),
+				},
+			},
+		],
+	}
+	presets[`mode_signage`] = {
+		type: 'button',
+		category: 'Mode',
+		name: `Signage`,
+		style: {
+			text: `Signage`,
+			size: '14',
+			color: combineRgb(255, 255, 255),
+			bgcolor: combineRgb(0, 0, 0),
+			show_topbar: 'default',
+		},
+		steps: [
+			{
+				down: [
+					{
+						actionId: 'currentMode',
+						options: {
+							mode: 'signage',
+						},
+					},
+				],
+				up: [],
+			},
+		],
+		feedbacks: [
+			{
+				feedbackId: 'currentMode',
+				options: {
+					mode: 'signage',
+				},
+				style: {
+					color: combineRgb(0, 0, 0),
+					bgcolor: combineRgb(0, 204, 0),
 				},
 			},
 		],
